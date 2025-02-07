fa9c864cd3b928b46e78038f22d9bf0a2c3982ff
AkmalxonAkmalxon <frontendonlinetutorials@gmail.com>
2025-02-07 10:05:46 +0500

    update Draggable.jsx

--- src/components/Draggable.jsx
+++ src/components/Draggable.jsx
@@ -3,25 +3,26 @@ import { CSS } from "@dnd-kit/utilities";
 import PropTypes from "prop-types";
 
 function Draggable(props) {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: props.id,
-  });
+  const { attributes, listeners, setNodeRef, transform, isDragging } =
+    useDraggable({
+      id: props.id,
+    });
+
   const style = {
     ...props.style,
-    // Outputs `translate3d(x, y, 0)`
     transform: CSS.Translate.toString(transform),
-    touchAction: 'none',
+    transition: isDragging ? "none" : "transform 0.3s ease",
+    touchAction: "none", // Предотвращает конфликты с тач-событиями
+    cursor: "grab",
+    // Применяем специальные стили во время перетаскивания
+    ...(isDragging && {
+      cursor: "grabbing",
+      zIndex: 999,
+    }),
   };
 
   return (
-    <div 
-      ref={setNodeRef} 
-      style={style} 
-      {...listeners} 
-      {...attributes}
-      role="button"
-      tabIndex={0}
-    >
+    <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
       {props.children}
     </div>
   );
